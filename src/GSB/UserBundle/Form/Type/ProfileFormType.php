ed631caee2119b144ed0ab240d8519a595c7cc64
Kevin RK57 RUBY <contact@kevinruby.me>
2015-04-13 18:42:28 +0200

    Édition de profil
    Possibilité de modifier son profil.
    Modification du README.

---
+++ src/GSB/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GSB\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder->add('nom')
+                ->add('prenom')
+                ->add('adresse')
+                ->add('cp')
+                ->add('ville')
+                ->add('dateEmbauche')
+                ->remove('current_password');
+    }
+    
+    public function getParent()
+    {
+        return 'fos_user_profile';
+    }
+
+    public function getName()
+    {
+        return 'gsb_user_profile';
+    }
+}
